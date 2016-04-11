c0608c5fd9f12f822424872e840348ad55cf6ed2
Taurus Colvin <taurus.colvin@gmail.com>
2016-04-11 01:09:16 -0400

    remove templates

--- client/hello-world.spec.js
+++ client/hello-world.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-addons-test-utils';
-import { chai } from 'meteor/practicalmeteor:chai';
+import { expect } from 'meteor/practicalmeteor:chai';
 import HelloWorld from './hello-world.jsx';
 
 describe('hello world component', function () {
@@ -11,10 +11,9 @@ describe('hello world component', function () {
     component = ReactTestUtils.renderIntoDocument(<HelloWorld />);
   });
 
-  it('displays hello world', function (done) {
+  it('displays hello world', function () {
     const node = ReactDOM.findDOMNode(component);
 
-    chai.assert.equal('Hello World', node.textContent, "does not contain 'Hello World'");
-    done();
+    expect(node.textContent).to.equal('Hello World');
   });
 });
